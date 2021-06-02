094d50084774c1714846f90339a2e9f087919a52
shintayunitar <shintayunita7@gmail.com>
2021-06-02 13:32:22 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::get('/', function () {
 Route::get('/admin', function () {return view('dashboard');})->name('admin');
 Route::get('/home', function () {return view('dashboardpenghuni');})->name('home');
 
-Route::get('/akun/kelola', function () {return view('akun.kelola');})->name('kontrak');
+Route::get('/akun', function () {return view('akun.index');})->name('akun');
+Route::get('/akun/kelola', function () {return view('akun.kelola');})->name('kelola');
+Route::get('/akun/buat',function () {return view('akun.buat');})->name('buat');
 
 Route::get('/kontrak', function () {return view('kontrak.index');})->name('kontrak');
 Route::get('/kontrak/buat', function () {return view('kontrak.buat');})->name('buat');
@@ -29,6 +31,11 @@ Route::get('/kontrak/aktif', function () {return view('kontrak.aktif');})->name(
 Route::get('/tagihan', function () {return view('tagihan.index');})->name('tagihan');
 Route::get('/tagihan/buat', function () {return view('tagihan.buat');})->name('buat');
 Route::get('/tagihan/edit', function () {return view('tagihan.edit');})->name('edit');
+
+Route::get('/kamar/buat', function () {return view('kamar.buat');})->name('buat');
+Route::get('/kamar/edit', function () {return view('kamar.edit');})->name('edit');
+Route::get('/kamar', function () {return view('kamar.index');})->name('kamar');
+
 
 
 
