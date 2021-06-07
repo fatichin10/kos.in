8e23fd101e8f520fd1a8f96f3bc8bed0a1f21574
fatichin10 <72029670+fatichin10@users.noreply.github.com>
2021-06-07 15:01:46 +0700

    Update index.blade.php

--- resources/views/tagihan/index.blade.php
+++ resources/views/tagihan/index.blade.php
@@ -3,23 +3,27 @@
 @section('konten')
 
     <style>
-        h3 {
+        h3,
+        h2 {
             font-weight: 600;
         }
 
-        label {
-            font-size: 18px;
-            font-weight: 600;
+        .status-aktif {
+            color: #578887;
+            font-weight: bold;
+            font-size: 26px;
         }
 
-        .isian {
-            border-color: #578887
+        .status-non-aktif {
+            color: #FC4E12;
+            font-weight: bold;
+            font-size: 26px;
         }
 
     </style>
 
     <div class="container" style="margin-top: 25px">
-        Kontrak Aktif / Tagihan Kontrak / xxxx / Edit Tagihan
+        Kontrak Aktif / Tagihan Kontrak
         <div class="row" style="margin-top: 10px">
             <div class="col-lg-1">
                 <a href="#">
@@ -27,60 +31,39 @@
                 </a>
             </div>
             <div class="col-lg-11" style="margin-top: 17px">
-                <h3>Edit Tagihan Kontrak</h3>
+                <h3 style="display: inline">Daftar Tagihan Kontrak</h3>
+                <a href="#" class="btn btn-success tombol" style="font-size: 18px"> + Buat Tagihan </a>
+            </div>
+        </div>
+    </div>
 
-                <!--Form-->
-                <div class="container background-box" style="padding-top: 30px">
-                    <form action="#" method="#">
-                        {{ csrf_field() }}
-                        <input type="submit" value="Simpan" class="btn btn-success tombol" style="margin-top: -70px">
-                        <div class="row">
-                            <div class="col-lg-2" style="margin-bottom: 30px">
-                                <input type="text" name="id" class="form-control btn-dark" value="xxxx" disabled>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="idkontrak" class="col-lg-3">ID Kontrak </label>
-                            <div class="col-lg-2">
-                                <input type="text" name="idkontrak" class="form-control isian" id="idkontrak" value="xxxx"
-                                    disabled>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="periode" class="col-lg-3">Periode Pembayaran <span
-                                    style="color: #FC4E12">*</span></label>
-                            <div class="col-lg-6">
-                                <input type="text" name="periode" class="form-control isian" id="periode"
-                                    value="Januari 2019" disabled>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="tgltagih" class="col-lg-3">Tanggal Penagihan <span
-                                    style="color: #FC4E12">*</span></label>
-                            <div class="col-lg-3">
-                                <input type="text" name="tgltagih" class="form-control isian" id="tgltagih"
-                                    value="18/01/2019" disabled>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="tglbayar" class="col-lg-3">Tanggal Pembayaran</label>
-                            <div class="col-lg-3">
-                                <input type="date" name="tgltagih" class="form-control isian" id="tgltagih">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="status" class="col-lg-3">Status </label>
-                            <div class="col-lg-3">
-                                <select name="status" class="form-control isian" id="status">
-                                    <option value="Belum Lunas">Belum Lunas</option>
-                                    <option value="Lunas">Lunas</option>
-                                </select>
-                            </div>
-                        </div>
-                    </form>
+    <br>
+
+    <div class="container background-box">
+        <div class="row">
+            <div class="col-lg-2" style="text-align: center">
+                <i class="iconify icon" data-inline="false" data-icon="uil:money-bill"
+                    style="color: #4e4b66; font-size: 50px;">
+                </i>
+                <h6>xxxx</h6>
+            </div>
+            <div class="col-lg-4">
+                <h3 style="margin-top: 20px">Januari 2019</h3>
+            </div>
+            <div class="col-lg-4">
+                <img src="/images/check.png"><span class="status-aktif">Lunas</span>
+                <h6 style="margin-left: 40px">18 Januari 2019</h6>
+            </div>
+            <div class="col-lg-1">
+                <div class="row">
+                    <a href="#" class="btn btn-primary btn-block">Edit</a>
+                </div>
+                <div class="row">
+                    <a href="#" class="btn btn-danger btn-block">Hapus</a>
                 </div>
             </div>
         </div>
+        <hr>
     </div>
 
 @endsection
